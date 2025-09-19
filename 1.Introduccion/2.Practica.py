5c2caaf45775a74827bfc8ebbbc5c3f5e887a702
Luz22310196 <160083181+Luz22310196@users.noreply.github.com>
2025-09-19 13:05:57 +0000

    3,Clases

--- 1.Introduccion/2.Practica.py
+++ 1.Introduccion/2.Practica.py
@@ -1,5 +1,16 @@
-#TEXTO CON UN SOLO ARGUMENTO
+# TEXTO CON UN SOLO ARGUMENTO
 print("No me repruebe profe")
 
-#TEXTO CON VARIOS ARGUMENTOS 
+# TEXTO CON VARIOS ARGUMENTOS Y ESPACIOS PREDETERMINADOS
 print("Ojala", "Luz", "Pase", "sus", "materias")
+
+# EL SEP NOS AYUDA A PERSONALIZAR LA SEPARACIÓN ENTRE PALABRAS
+print("Ojala", "Luz", "Pase", "sus", "materias", sep="-")
+
+# SE PERSONALIZA EL FIN DE LA LÍNEA DE TEXTO
+print("Ojala", "Luz", "Pase", "sus", "materias", end="!")
+
+# IMPRIMIR DOS MENSAJES EN LA MISMA LÍNEA
+print("Ojala", "Luz", "Pase", "sus", "materias", end=" ")
+print("y", "que", "no", "tenga", "ningún", "problema.")
+

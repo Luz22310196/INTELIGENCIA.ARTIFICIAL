789f84e12e4711c81c9efad5fdc2ea20b4b63341
Luz22310196 <160083181+Luz22310196@users.noreply.github.com>
2025-09-19 17:19:48 +0000

    8,Modulos

---
+++ 2.Avanzada/7.Modulos.py
@@ -0,0 +1,18 @@
+#MODULOS INTEGRADOS
+#cadena = "Hola, mundo!"
+#longitud = len(cadena)
+#print(longitud)
+
+# IMPORTAMOS EL MÓDULO RANDOM (VIENE CON PYTHON)
+import random
+
+# GENERAMOS UN NÚMERO ALEATORIO ENTRE 1 Y 1000
+aleatorio = random.randint(1, 1000)
+
+# IMPRIMIMOS EL NÚMERO GENERADO
+print(aleatorio)
+
+# MOSTRAMOS LOS NOMBRES DEFINIDOS EN EL ENTORNO ACTUAL
+print(dir())
+
+
---
+++ 2.Avanzada/8.PracticaSuma/1.PracticaSuma.py
@@ -0,0 +1,9 @@
+# Importa el módulo suma (debe estar en la misma carpeta)
+import Suma
+
+# Usa la función sumar del módulo suma
+resultado = Suma.sumar(10, 22)
+print(resultado)
+
+
+
